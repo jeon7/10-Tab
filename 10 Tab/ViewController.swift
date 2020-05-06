d4a235083a731d48dfc7aaa48afcb0adc967ac06
Gukhwa Jeon <jeon.gukhwa7@gmail.com>
2020-05-07 00:05:31 +0200

    Initial Commit

---
+++ 10 Tab/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  10 Tab
+//
+//  Created by Gukhwa Jeon on 07.05.20.
+//  Copyright © 2020 G-Kay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
